3d1f12f5069d8ac3368f65e236498c627dc61d4d
Sara Cipriano <130059301+SaraGuaiume@users.noreply.github.com>
2026-09-24 15:29:38 -0300

    Update exercicio7.C

--- Alg51 Sara Guaiume/PrimeirosExercicios/exercicio7.C
+++ Alg51 Sara Guaiume/PrimeirosExercicios/exercicio7.C
@@ -1,17 +1,17 @@
-// SETE - Leia 15 números inteiros e armazene-os em um vetor. Ao final, informe quantos números são pares e quantos são ímpares.
+// SETE - Leia tamanVetor números inteiros e armazene-os em um vetor. Ao final, informe quantos números são pares e quantos são ímpares.
 
 #include <stdio.h>
 
- void armazenarValores(int vetorValores[15]) {
-    for (int i = 0; i < 15; i++) {
+ void armazenarValores(int vetorValores[tamaVetor]) {
+    for (int i = 0; i < tamanhoVetor; i++) {
         printf("Digite o valor %d: ", i);
         scanf("%d", &vetorValores[i]);
     }
    
 }
 
-int classificarParImpar(int vetorValores[15], int contarPares) {
-    for (int i = 0; i < 15; i++) {
+int classificarParImpar(int vetorValores[tamanhoVetor], int contarPares) {
+    for (int i = 0; i < tamanhoVetor; i++) {
         if (vetorValores[i] % 2 == 0) {
             contarPares = contarPares + 1;
         } 
@@ -20,14 +20,15 @@ int classificarParImpar(int vetorValores[15], int contarPares) {
 }
 
 int main() {
-    int vetor[15];
+    int tamanhoVetor = tamanVetor;
+    int vetor[tamanhVetor];
     int contarPares = 0;
     int qtdPares = 0;
     int qtdImpares = 0;
 
     armazenarValores(vetor);
     qtdPares = classificarParImpar(vetor, contarPares);
-    qtdImpares = 15 - qtdPares;
-    printf("A quantidade de numeros pares e impares eh %d, %d, respactivamente", qtdPares, qtdImpares);
+    qtdImpares = tamanhoVetor - qtdPares;
+    printf("A quantidade de numeros pares e impares eh %d e %d, respactivamente", qtdPares, qtdImpares);
 
 }
